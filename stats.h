fd1a920ed4b6bc8b224d4a61dc960741f0c92d78
Kishor-Pachha182 <100278012+Kishor-Pachha182@users.noreply.github.com>
2022-03-04 15:18:36 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -4,6 +4,11 @@
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
 
+typedef struct {
+  int avg;
+  int min;
+  int max;
+}stats;
 struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
